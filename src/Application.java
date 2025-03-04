23c9a8dccbbbcb00724d73c1cff9466fda84d7a7
qus0in <qus0in@gmail.com>
2025-03-04 12:46:41 +0900

    Slack

--- src/Application.java
+++ src/Application.java
@@ -1,21 +1,18 @@
+import slack.Slack;
 import util.logger.MyLogger;
 import util.logger.MyLoggerLevel;
+import util.secret.MySecret;
+import util.secret.NoEnvException;
+import util.secret.SecretCategory;
 import util.webclient.WebClient;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
-    public static void main(String[] args) {
-        MyLogger logger = MyLogger.getLogger();
-        logger.setLevel(MyLoggerLevel.DEBUG);
-        WebClient webClient = WebClient.getWebClient();
-        String result = "";
-        try {
-            result = webClient.sendRequest("", webClient.makeRequest(new HashMap<>()));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        logger.info(result);
+    public static void main(String[] args) throws NoEnvException {
+        Slack slack = new Slack();
+        slack.sendMessage("안녕 나는 슬랙이야 헬륨가스 마시고 요로케됐지");
     }
 }
--- src/util/webclient/WebClient.java
+++ src/util/webclient/WebClient.java
@@ -1,5 +1,7 @@
 package util.webclient;
 
+import util.logger.MyLogger;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -9,8 +11,11 @@ import java.util.Map;
 
 public class WebClient implements IWebClient {
     private final HttpClient httpClient;
+    private final MyLogger logger;
     private WebClient() {
         httpClient = HttpClient.newHttpClient();
+        logger = MyLogger.getLogger();
+        logger.info("WebClient 생성");
     }
     private static WebClient instance;
 
@@ -22,23 +27,26 @@ public class WebClient implements IWebClient {
     }
 
     @Override
-    public String sendRequest(String path, HttpRequest request) throws IOException, InterruptedException {
+    public String sendRequest(HttpRequest request) throws IOException, InterruptedException {
+        logger.debug("Request 요청 전달 시도");
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        logger.debug(Integer.toString(response.statusCode()));
         return response.body();
     }
 
     @Override
     public HttpRequest makeRequest(Map<String, String> map) {
+        logger.debug("Request 객체 생성 시도");
         return HttpRequest.newBuilder()
                 .uri(URI.create(map.get("url")))
                 .method(map.get("method"), HttpRequest.BodyPublishers.ofString(map.get("body")))
-                .headers(map.get("headers").split(";"))
+//                .headers(map.get("headers").split(";"))
                 .build();
     }
 }
 
 interface IWebClient {
-    String sendRequest(String path, HttpRequest request) throws IOException, InterruptedException;
+    String sendRequest(HttpRequest request) throws IOException, InterruptedException;
 
     HttpRequest makeRequest(Map<String, String> map);
 }
