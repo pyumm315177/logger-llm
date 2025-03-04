0f09762a4ffa1827f92964b85f77b63a00392ed1
qus0in <qus0in@gmail.com>
2025-03-04 12:50:51 +0900

    Sleep

--- src/Application.java
+++ src/Application.java
@@ -9,10 +9,18 @@ import util.webclient.WebClient;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
 
 public class Application {
-    public static void main(String[] args) throws NoEnvException {
+    public static void main(String[] args) throws NoEnvException, InterruptedException {
         Slack slack = new Slack();
-        slack.sendMessage("안녕 나는 슬랙이야 헬륨가스 마시고 요로케됐지");
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(1000);
+            slack.sendMessage("안녕안녕 나는 슬랙이야");
+            Thread.sleep(1000);
+            slack.sendMessage("헬륨가스 마시고");
+            Thread.sleep(1000);
+            slack.sendMessage("요를레이히~");
+        }
     }
 }
